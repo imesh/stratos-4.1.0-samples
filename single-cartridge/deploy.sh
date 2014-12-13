53e77dc8b85e321e6b3f010c7122e0d8e4ae0e77
Imesh Gunaratne <imesh@apache.org>
2014-12-13 18:58:20 +0530

    Adding single cartridge application

---
+++ single-cartridge/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh 
+
+curl -X POST -H "Content-Type: application/json" -d @'artifacts/autoscale-policy.json' -k -v -u admin:admin https://localhost:9443/api/autoscalingPolicies
+
+curl -X POST -H "Content-Type: application/json" -d @'artifacts/tomcat-cartridge.json' -k -v -u admin:admin https://localhost:9443/api/cartridges
+
+sleep 5
+
+curl -X POST -H "Content-Type: application/json" -d @'artifacts/application.json' -k -v -u admin:admin https://localhost:9443/api/applications
+
+sleep 5 
+curl -X POST -H "Content-Type: application/json" -d@'artifacts/deployment-policy.json' -k -v -u admin:admin https://localhost:9443/api/applicationDeployments
+

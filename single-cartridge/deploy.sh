3dec5ea2e7775a80e3719e844e3cdd0d27bb8ca2
Imesh Gunaratne <imesh@apache.org>
2014-12-16 01:36:29 +0530

    Updating artifacts according to application deployment refinements

--- single-cartridge/deploy.sh
+++ single-cartridge/deploy.sh
@@ -2,6 +2,8 @@
 
 curl -X POST -H "Content-Type: application/json" -d @'artifacts/autoscale-policy.json' -k -v -u admin:admin https://localhost:9443/api/autoscalingPolicies
 
+sleep 1
+
 curl -X POST -H "Content-Type: application/json" -d @'artifacts/tomcat-cartridge.json' -k -v -u admin:admin https://localhost:9443/api/cartridges
 
 sleep 5
@@ -9,5 +11,5 @@ sleep 5
 curl -X POST -H "Content-Type: application/json" -d @'artifacts/application.json' -k -v -u admin:admin https://localhost:9443/api/applications
 
 sleep 5 
-curl -X POST -H "Content-Type: application/json" -d@'artifacts/deployment-policy.json' -k -v -u admin:admin https://localhost:9443/api/applicationDeployments
+curl -X POST -H "Content-Type: application/json" -d@'artifacts/deployment-policy.json' -k -v -u admin:admin https://localhost:9443/api/application/single-cartridge-app/deploy
 
